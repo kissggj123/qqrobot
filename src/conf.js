c0dea89dfe8decee16f8bcae7d86c75ca1bc4ed7
fumeboy <fumeboy@qq.com>
2019-06-11 18:01:37 +0800

    定时任务

--- src/conf.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    dbPath: 'mongodb://127.0.0.1:27017/qqrobot',
-    wsPath: 'ws://129.28.148.203:49632/',
-    // group: [534116445, 931851790,763598652]
-    group: [534116445],
-    bing_pic_path: "./out.jpg",
-    morning_pic_path:'C:\\Users\\Administrator\\Desktop\\QQLight\\QQLight\\img\\morning.jpg'
-};
